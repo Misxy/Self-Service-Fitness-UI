1773b229cfc25826dcfe62e25524f1002922d691
Supakorn Chupraphawan <supakorn.chupraphawan@outlook.com>
2023-04-16 16:29:31 +0700

    Update client UI and validators (#3)
    * Update .gitignore
    
    * Update angular.json
    
    * Update package.json and package-lock.json files.
    
    * Update registeration.component.ts
    
    * Update codes.
    
    * Added the validators requireTrue to the checkbox form.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { EnsureModuleLoadedOnceGuard } from './guards/ensure-module-loaded-once-guard.guard';
+import { ClientService } from './services/client.service';
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [],
-  providers: [],
+  providers: [ClientService],
 })
 export class CoreModule extends EnsureModuleLoadedOnceGuard {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -4,3 +4,4 @@ export { FooterComponent } from './layouts/footer/footer.component';
 export { ScrollableModalComponent } from './layouts/scrollable-modal/scrollable-modal.component';
 export { ModalComponent } from './layouts/modal/modal.component';
 export { DisplayMessage } from './models/displayMessage/display-message.model';
+export { MemberBuilder } from './models/registration/member.model';
